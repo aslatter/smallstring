089d6e92d628d5c9bb8eb32dbcb5dec13b7f4694
Antoine Latter <aslatter@gmail.com>
2011-01-25 02:13:05 +0000

    update benchmarks for Text

--- bench.sh
+++ bench.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 pushd test || exit 0
 rm -f *.hi *.o Bench
-ghc --make -O2 -package-conf ../pkg.conf.d Bench.hs -o Bench || popd; exit 0 }
+ghc -Wall --make -O2 -package-conf ../pkg.conf.d Bench.hs -o Bench || popd; exit 0 }
 ./Bench
 popd > /dev/null
 
--- test/Bench.hs
+++ test/Bench.hs
@@ -12,26 +12,37 @@ import Criterion.Main
 import qualified Data.Set as Set
 import Data.List (nub)
 
+import qualified Data.Text as T
+import qualified Data.ByteString.Char8 as B
+
 getStrings :: IO [String]
 getStrings = lines `fmap` readFile "words.txt"
 
 getSmallStrings :: IO [SmallString]
 getSmallStrings =  map SS.fromString `fmap` getStrings
 
+getTexts :: IO [T.Text]
+getTexts = map T.pack `fmap` getStrings
+
 main = do
   strings <- getStrings
   smallStrings <- getSmallStrings
+  texts <- getTexts
 
   let testSmallString = fromString "hello, world!"
 
   defaultMain
     [ bench "building set of strings" $
             nf Set.fromList strings
+    , bench "building a set of texts" $
+            nf Set.fromList texts
     , bench "building set of small strings" $
             nf Set.fromList smallStrings
 
     , bench "nub on strings" $
             nf nub strings
+    , bench "nub on texts" $
+            nf nub texts
     , bench "nub on small strings" $
             nf nub smallStrings
 
@@ -39,5 +50,13 @@ main = do
             nf fromString "hello, world!"
     , bench "toString \"hello, world!\"" $
             nf toString testSmallString
+
+    , bgroup "conversions"
+            [ bench "fromText" $ nf fromText testText
+            ]
     ]
 
+str = "the quick brown fox jumped over the lazy dog"
+
+testText :: T.Text
+testText = T.pack str
